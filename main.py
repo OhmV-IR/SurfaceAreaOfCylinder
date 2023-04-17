c553962e9cf4f11af116d242b25d034b1be1cdd5
AB <bourdeaux.adrien@gmail.com>
2023-04-17 13:57:29 -0400

    fixed error

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ while(running):
     height = input("What is the height of the cylinder: ")
     r = input("What is the radius of the circle: ")
     try:
-        float(r)
-        float(height)
+        r = float(r)
+        height = float(height)
     except:
         print("input is not an integer please retry")
         exit()
     C = float(2 * r) * pi
-    RectangleA = round(C * float(height))
+    RectangleA = round(C * height)
     CircleA = round(float(r**2) * pi)
     print("Surface area of the cylinder: " + str(RectangleA) + str(CircleA))
     running = False;
